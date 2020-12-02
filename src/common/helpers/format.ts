df354b31db83a4632ea7696eb9241028a7398e5a
Igor Matias <matyas.igor@gmail.com>
2020-12-03 00:43:04 +0300

    fix: replace power operation for BigInt

--- src/common/helpers/format.ts
+++ src/common/helpers/format.ts
@@ -1,3 +1,16 @@
+// '**' operation for BigInt's is broken on transpiling level, so I have to replace it
+// TypeError: Cannot convert a BigInt value to a number
+//     at Math.pow (<anonymous>)
+const power = (x: bigint, y: bigint) => {
+  let result = BigInt(x)
+  let exponent = BigInt(y)
+  while (exponent > 1n) {
+    result = result * BigInt(x)
+    exponent -= 1n
+  }
+  return result
+}
+
 export const formatValue = (number: number | BigInt, currency: string = 'BTC'): string => {
   if (number === undefined || number === null) {
     return ''
@@ -15,6 +28,6 @@ const HALVINGS_INTERVAL = 210000n
 
 export const getBlockReward = (blockHeight: number, currency: string = 'BTC'): string => {
   const halvingsAmount = BigInt(blockHeight) / HALVINGS_INTERVAL
-  const currentReward = INITIAL_REWARD / 2n ** halvingsAmount
+  const currentReward = INITIAL_REWARD / power(2n, halvingsAmount)
   return formatValue(currentReward, currency)
 }
