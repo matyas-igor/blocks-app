a6604b2bfa80e26df68728760ffb08eb7a38abd5
Igor Matias <matyas.igor@gmail.com>
2020-12-03 00:22:18 +0300

    fix: simplify BigInt processing

--- src/common/helpers/format.ts
+++ src/common/helpers/format.ts
@@ -1,19 +1,16 @@
-const POWER = 8n
-const POWER_NUMBER = 8
-
 export const formatValue = (number: number | BigInt, currency: string = 'BTC'): string => {
   if (number === undefined || number === null) {
     return ''
   }
   const bigNumber = BigInt(number)
-  const floatPart = bigNumber % 10n ** POWER
+  const floatPart = bigNumber % 10n ** 8n
   const integerPart = bigNumber - floatPart
-  return `${(integerPart / 10n ** POWER).toLocaleString()}.${floatPart.toString().padStart(POWER_NUMBER, '0')}${
+  return `${(integerPart / 10n ** 8n).toLocaleString()}.${floatPart.toString().padStart(8, '0')}${
     currency ? ` ${currency}` : ''
   }`
 }
 
-const INITIAL_REWARD = 50n * 10n ** POWER
+const INITIAL_REWARD = 50n * 10n ** 8n
 const HALVINGS_INTERVAL = 210000n
 
 export const getBlockReward = (blockHeight: number, currency: string = 'BTC'): string => {
